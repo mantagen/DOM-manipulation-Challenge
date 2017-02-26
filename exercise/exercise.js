beb6dafff6f8b0f9a0f89641a0c7e91bc5514712
mantagen <mathsgregory@gmail.com>
2017-02-26 14:26:09 +0200

    Editing exercise

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,33 +1,39 @@
-/* ARGUMENTS SHOULD NOT BE MUTATED */
+/*
+Disclaimer: some of these functions are very specific, and in real life
+might be generalised to be more useful.
+*/
+
+
+/* EXERCISES */
 
 /*
 Write a function which adds a class "highlight" to the introduction paragraph
 */
 var highlightText = function(){
-
-}
+  document.querySelector(".introduction").classList.add("highlight");
+};
 
 /*
 Write a function which removes the class "highlight" from the introduction
 paragraph
 */
-var unHighlightText = function(){
-
-}
+var unHighlightIntroText = function(){
+ document.querySelector(".introduction").classList.remove("highlight");
+};
 
 /*
 Write a function which, if the introduction paragraph has the class "highlight",
 it will be removed; otherwise the class "highlight" is added.
 */
 var toggleHighlight = function(){
-
+  document.querySelector(".introduction").classList.toggle("highlight");
 }
 
 /*
 Write a function which returns all text content of the introduction paragraph
 */
 var getText = function(){
-
+  return document.querySelector(".introduction").innerText;
 }
 
 /*
@@ -35,14 +41,14 @@ Write a function which returns the number of charachters in the introduction
 paragraph
 */
 var getNumChars = function(){
-
+  return getText().length;
 }
 
 /*
 Write a function which returns the number of words in the introduction paragraph
 */
 var getNumWords = function(){
-
+  return getText().split(" ").length;
 }
 
 /*
@@ -50,22 +56,36 @@ Write a function which returns the number of sentences in the introduction
 paragraph
 */
 var getNumSentences = function(){
+  return getText().split(".").length;
+}
 
+/*
+Write a function which returns the average number of words per sentence
+*/
+var getWordsPerSentece = function(){
+  return getNumSentences()/getNumWords();
+}
+
+/*
+Write a function which returns the average number of letters per word
+*/
+var getLettersPerWord = function(){
+  return getNumChars()/getNumWords();
 }
 
 /*
 Write a function which takes a string, and returns true if the introduction
 paragraph contains that string, and false if it doesn't.
 */
-var containsString = function(){
-
+var containsString = function(string){
+  return document.querySelector(".introduction").indexOf(string) === -1 ? false : true;
 }
 
 /*
 Write a function which returns the value in the `firstName` text input
 */
 var getFirstNameValue = function(){
-
+  return document.querySelector("input[name='firstName']").value;
 }
 
 /*
@@ -73,7 +93,7 @@ Write a function which takes a string, `inputName`, and returns the value in the
 has `inputName` as the name attribute
 */
 var getValue = function(inputName){
-
+  return document.querySelector("input[name='"+inputName+"']").value;
 }
 
 /*
@@ -82,6 +102,11 @@ and returns a copy of the object, but with an `inputName` key on the object
 whose value is taken from the input element with that name.
 */
 var updateStateValue(formState, inputName){
+  // return Object.assign({}, formState, {inputName: getValue(inputName)});
+  var stateCopy = Object.keys(formState).reduce(function(accumulator, key){
+    accumulator[key] = formState[key];
+  },{});
+  stateCopy[inputName] = getValue(inputName);
 
 }
 
@@ -91,15 +116,26 @@ Write a function which takes an object,`formState`, and an array of string,
 input with name in `inputNames` array.
 */
 var updateStateValues(formState, inputNames){
-
-}
+  var stateToAdd = inputNames.reduce(function(acc, inputName){
+    acc[inputName] = getValue(inputName);
+    return acc;
+  },{});
+  var stateCopy = Object.keys(formState).reduce(function(accumulator, key){
+    accumulator[key] = formState[key];
+  },{});
+  var newState = Object.keys(stateToAdd).reduce(function(accumulator, key){
+    accumulator[key] = formState[key];
+  },stateCopy);
+  return newState;
+};
 
 /*
-Write a function which returns an **array** of values of inputs with a given class
+Write a function which returns an **array** of values of all input elements
+with a given class
 */
 var getInputValues(className){
-
-}
+  Array.prototype.slice.call(document.querySelectorAll("input."className));
+};
 
 /*
 Write a function which takes a className, and returns the number of elements in
@@ -107,17 +143,27 @@ the DOM with that className. If the function is passed an argument which doesn't
 have type `string`, the function should return 0;
 */
 var getNumElsOfClass = function(className){
-
-}
+  return document.querySelectorAll(className).length;
+};
 
 /*
 Write a function, `generateUl`, which takes an array of strings, and returns a
 `ul` htmlElement containing `li` elements for each array element. Each `li'
 should contain the value of the array element.
 */
-var generateUl = function(array){
-
-}
+var generateUl = function(arr){
+  var ulNode = document.createElement("ul");
+  var liNode;
+  var textNode;
+  arr.forEach(elem, function(){
+    if(typeof elem === "string" || typeof elem === "integer");
+    liNode = document.createElement("li");
+    textNode = document.createTextNode(elem);
+    liNode.appendChild(textNode);
+    ulNode.appendChild(liNode);
+  });
+  return ulNode;
+};
 
 /*
 Write a function, `generateNestedUl`, which takes array whose elements can be
